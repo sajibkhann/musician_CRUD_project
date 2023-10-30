fe9e5b2de0e9df9527180f21eada884992958304
Sajib khan <sajibkhan.bubt@gmail.com>
2023-10-30 13:07:53 +0600

    Files Added

---
+++ first_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Musician (models.Model):
+    first_name = models.CharField (max_length=50)
+    last_name = models.CharField (max_length=50)
+    instrument = models.CharField (max_length=100)
+
+    def __str__(self) :
+        return self.first_name +" "+ self.last_name+" "+self.instrument
+
+class Album (models.Model):
+    artist = models.ForeignKey (Musician, on_delete=models.CASCADE)
+    name = models.CharField (max_length=50)
+    release_date = models.DateField ()
+    ratings = (
+        (1, "Worst"),
+        (2, "Bad"),
+        (3, "Average"),
+        (4, "Good"),
+        (5, "Excellent")
+    )
+
+    num_stars = models.IntegerField (choices=ratings)
+    def __str__(self):
+        return self.name+"Ratings: "+str(self.num_stars)
